66305deda0fdae3dd86f207f15e2d8414554a35e
diyor.adawev <adashevdiyor443@gmail.com>
2025-05-01 12:08:11 +0500

    home page full done.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,35 @@
+import './style.css'
+import Logo from '../../assets/logo/logo.png'
+import {Link} from "react-router-dom";
+import Instagram from '../../assets/icons/instagram.svg'
+import Facebook from '../../assets/icons/facebook.svg'
+import Linkedin from '../../assets/icons/linkedin.svg'
+import Google from '../../assets/icons/google.svg'
+
+function Footer(){
+    return <div className="footer-page">
+        <div className="company-info">
+            <img src={Logo} alt="Logo"/>
+            <p>Stay updated with the latest laptops, deals, and tech news. Be the first to know about new models, sales, and special promos.</p>
+        </div>
+        <div className="about">
+            <p className={'title'}>About</p>
+            <Link to={'/'}><p className={'about-items f-item'}>Home</p></Link>
+            <Link to={'/shop'}><p className={'about-items'}>Shop</p></Link>
+            <Link to={'/faq'}><p className={'about-items'}>FAQs</p></Link>
+            <Link to={'/stories'}><p className={'about-items'}>Stories</p></Link>
+        </div>
+        <div className="contact-us">
+            <p className={'title'}>Contact us</p>
+            <p className={'txt-info f-info-txt'}>+998 99 777 77 77</p>
+            <p className={'txt-info'}>noutstore@org.com</p>
+            <div className="socials">
+                <Link to={'/instagram'}><img src={Instagram} alt=""/></Link>
+                <Link to={'/facebook'}><img src={Facebook} alt=""/></Link>
+                <Link to={'/linkedin'}><img className={'lin-img'} src={Linkedin} alt=""/></Link>
+                <Link to={'/google'}><img className={'google-img'} src={Google} alt=""/></Link>
+            </div>
+        </div>
+    </div>
+}
+export default Footer;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import Layout1 from "../../components/Layout1/Layout1";
 import LayoutTopPicks from "../../components/LayoutTopPicks/LayoutTopPicks";
 import LayoutCenter from "../../components/LayoutCenter/LayoutCenter";
 import './style.css'
+import Footer from "../../components/Footer/Footer";
 
 function Home(){
     return <div className={'home-page'}>
@@ -10,6 +11,7 @@ function Home(){
         <Layout1/>
         <LayoutTopPicks/>
         <LayoutCenter/>
+        <Footer/>
     </div>
 }
 export default Home;
